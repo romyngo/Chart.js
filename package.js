0dc8235381fe7970beea6b98f98e0eceed56b5f4
romyngo <ngominhquan88@gmail.com>
2016-02-12 16:30:25 +0700

    Added meteor packaging

---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'romyngo:chartjs',
+  version: '1.0.2',
+  // Brief, one-line summary of the package.
+  summary: 'Chart.js meteor wrapper',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/romyngo/Chart.js.git',
+  documentation: 'README.md',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.addFiles('chartjs.js');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('romy:chartjs');
+  api.addFiles('chartjs-tests.js');
+});
